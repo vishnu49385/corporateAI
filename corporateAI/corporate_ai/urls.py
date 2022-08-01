b20ed4d58305b5a98bd6428301179ee8d7d10662
dev-abhijith <abhijith2040@gmail.com>
2022-08-01 16:12:23 +0530

    added templates

--- corporateAI/corporate_ai/urls.py
+++ corporateAI/corporate_ai/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name= 'index'),
+    path('products/', views.products, name='products'),
+    path('services/', views.services, name='services'),
+    path('contact-us/', views.contact, name='contact'),
+    path('about-us/', views.about, name='about')
 ]
--- corporateAI/corporate_ai/views.py
+++ corporateAI/corporate_ai/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello iasdffadfndex")
+    return render(request, 'index.html')
+
+def products(request):
+    pass
+
+def services(request):
+    pass
+
+def contact(request):
+    pass
+
+def about(request):
+    pass
