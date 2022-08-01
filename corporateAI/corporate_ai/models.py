1fcb9fa8417fc1c88178c74ed010e14245c608c1
dev-abhijith <abhijith2040@gmail.com>
2022-08-01 15:53:11 +0530

    added products model

--- corporateAI/corporate_ai/models.py
+++ corporateAI/corporate_ai/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Products(models.Model):
+    title = models.CharField(max_length= 200)
+    description = models.CharField(max_length= 2000)
+    # image = models.ImageField(upload_to='product_images', null = True, blank = True)
+
