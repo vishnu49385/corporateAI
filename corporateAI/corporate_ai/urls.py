93d7813bb30eeb4f68a3c323deebf3be1fde0dbd
dev-abhijith <abhijith2040@gmail.com>
2022-08-07 19:46:48 +0530

    adding background image

--- corporateAI/corporate_ai/urls.py
+++ corporateAI/corporate_ai/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name= 'index'),
+    path('products/cad-design/', views.cad_detail, name='cad'),
+    path('products/graphic-design/', views.graphic_detail, name='graphic-design'),
+    path('products/carbon-capture-equipment/', views.cce_detail, name='cce'),
     path('products/', views.products, name='products'),
     path('services/', views.services, name='services'),
     path('contact-us/', views.contact, name='contact'),
--- corporateAI/corporate_ai/views.py
+++ corporateAI/corporate_ai/views.py
@@ -1,17 +1,51 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        "page":"index",
+    }
+    return render(request, 'index.html', context)
 
 def products(request):
-    return render(request, 'products.html')
+    context = {
+        "page":"products",
+    }
+    return render(request, 'products.html',context)
+
+def cad_detail(request):
+    context = {
+        "page":"products",
+    }
+    return render(request, 'cad.html',context)
+
+def cce_detail(request):
+    context = {
+        "page":"products",
+    }
+    return render(request, 'cce.html',context)
+
+def graphic_detail(request):
+    context = {
+        "page":"products",
+    }
+    return render(request, 'graphic-design.html',context)
 
 def services(request):
-    return render(request, 'services.html')
+    context = {
+        "page":"services",
+    }
+    return render(request, 'services.html',context)
 
 def contact(request):
-    return render(request, 'contact.html')
+    context = {
+        "page":"contact",
+    }
+    return render(request, 'contact.html',context)
 
 def about(request):
-    return render(request, 'about.html')
+    context = {
+        "page":"about",
+    }
+    return render(request, 'about.html',context)
