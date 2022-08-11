1a45c2ddf698ca4ab0164fe0c2f956050da6a4cc
dev-abhijith <abhijith2040@gmail.com>
2022-08-11 23:09:24 +0530

    allowed hosts changed

--- corporateAI/corporateAI/settings.py
+++ corporateAI/corporateAI/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@!(311@5(8c563u4e3q7-30u!b+mmpj@k)kjvcg4kfp(cq%^$%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['13.57.12.91']
+ALLOWED_HOSTS = []
 
 
 # Application definition
