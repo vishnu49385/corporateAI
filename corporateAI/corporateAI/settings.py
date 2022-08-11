84a03e6a36018628e60d1124ab867cee0aff8572
dev-abhijith <abhijith2040@gmail.com>
2022-08-11 23:38:57 +0530

    deploy test

--- corporateAI/corporateAI/settings.py
+++ corporateAI/corporateAI/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@!(311@5(8c563u4e3q7-30u!b+mmpj@k)kjvcg4kfp(cq%^$%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -118,6 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATIC_URL = 'static/'
 
