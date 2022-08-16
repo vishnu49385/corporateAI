a6a96992cdfad46b5c249706959ced623bbb94bd
dev-abhijith <abhijith2040@gmail.com>
2022-08-16 10:05:57 +0530

    debug issue

--- corporateAI/settings.py
+++ corporateAI/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@!(311@5(8c563u4e3q7-30u!b+mmpj@k)kjvcg4kfp(cq%^$%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
